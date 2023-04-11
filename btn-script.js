fcfe381c162ae5a9c3fe1c6f7e2f696f769d1566
hpo2004 <hpo2004@nyu.edu>
2023-04-11 08:16:02 -0400

    button assignment

--- btn-script.js
+++ btn-script.js
@@ -15,7 +15,7 @@ btn.addEventListener("click", () => {
         const windowWidth = window.innerWidth;
         const windowHeight = window.innerHeight;
         const randomX = Math.floor(Math.random() * (windowWidth - 25));
-        const randomY = Math.floor(Math.random() * windowHeight - 100);
+        const randomY = Math.floor(Math.random() * windowHeight - 200);
         btn.style.left = `${randomX}px`;
         btn.style.top = `${randomY}px`;
         console.log(randomX, randomY);
